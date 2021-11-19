e0a2cf1310be992363f0199157c4567d82ffb77d
Elumalai kannan <53072890+Elumalai04@users.noreply.github.com>
2021-11-19 03:10:25 -0700

    FirstCommit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World! HCSC PROJECT");
+        }
+    }
+}
